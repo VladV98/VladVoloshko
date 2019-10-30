39c35e09fab60ee210f9cb633cdc23ddd31a8261
Opilane TTHK <opilane@tthk.ee>
2019-10-30 13:19:41 +0200

    dbLesson 30.10.19

--- dbLesson/ShopDatabase/Program.cs
+++ dbLesson/ShopDatabase/Program.cs
@@ -54,23 +54,89 @@ namespace ShopDatabase
                         Console.WriteLine($"Name: {food.Name}  Price: {food.Price}");
                     }
                     Console.WriteLine($"Total: {cart.Sum}");
+
+
                 }
+                var cartsi = db.ShoppingCarts;
+                var cartsWithItems = db.ShoppingCarts.Include("Items");
+                var foods = db.Foods;
+
+                //1. Last ctrated cart
+                var latest = cartsWithItems.OrderByDescending(cart => cart.DateCreated).ToList().First();
+                var latest2 = cartsWithItems.OrderBy(cart => cart.DateCreated).ToList().Last();
+
 
                 Console.WriteLine();
-                Console.WriteLine("1. Show only the last(latest created) shopping cart with all its items");
+                Console.WriteLine($"Shopping cart created on {latest.DateCreated}");
+                Console.WriteLine($"Shopping cart created on {latest2.DateCreated}");
+
+                //2. Carts with Sum > 5
+                var carts5 = carts.Where(x => x.Sum > 5).ToList();
+                Console.WriteLine();
+                foreach (var cart in carts5)
+                {
+                    Console.WriteLine($"Shopping cart created on {cart.DateCreated} Sum: {cart.Sum}");
+                }
+
+                //3.Show only the carts with more than one item in it(with the number of items)
+                var cartsMoreThan1 = cartsWithItems.Where(x => x.Items.Count() > 1).ToList();
+                Console.WriteLine();
+                foreach (var cart in cartsMoreThan1)
+                {
+                    Console.WriteLine($"Shopping cart created on {cart.DateCreated} Item count: {cart.Items.Count()}");
+                }
+                var cartsMoreThan1_query = from cart in cartsWithItems where cart.Items.Count() > 1 select cart;
+                foreach (var cart in cartsMoreThan1_query)
+                {
+                    Console.WriteLine($"Shopping cart created on {cart.DateCreated} Item count: {cart.Items.Count()}");
+                }
+
+                //4. Show only the carts that contain apples
+                var cartsWithApples = cartsWithItems.Where(x => x.Items.Any(y => y.Name == "apple"));
+                Console.WriteLine();
+                foreach (var cart in cartsWithApples)
+                {
+                    Console.WriteLine($"Shopping cart created on {cart.DateCreated}");
+                    foreach(var food in cart.Items)
+                    {
+                        Console.WriteLine($"Name: {food.Name}");
+                    }
+                }
+
+                //5. Show the total number of shopping carts
+                Console.WriteLine();
+                var count = carts.Count();
+                Console.WriteLine($"Total number of carts is {count}");
+
+                //6. Show the cart with maximum sum
+                Console.WriteLine();
+                var cartWithMaximumSum = carts.OrderByDescending(x => x.Sum).FirstOrDefault();
+                Console.WriteLine($"Cart created on {cartWithMaximumSum.DateCreated} Sum: {cartWithMaximumSum.Sum}");
+
+                //7. Show the cheapest food
+                Console.WriteLine();
+                var cheapestFood = foods.OrderByDescending(food => food.Price).ToList().Last();
+                Console.WriteLine($"Cheapest food is {cheapestFood.Name} Price: {cheapestFood.Price}");
+
+                //Console.WriteLine("1. Show only the last(latest created) shopping cart with all its items");
 
 
-                IEnumerable <Food> Cart1 =
+                /*IEnumerable <Food> Cart1 =
                     from name in db.Foods
                     select name;
-                Console.WriteLine(Cart1.First());
+                foreach (var cart in Cart1)
+                {
+                    Console.WriteLine(cart.Name.Last(), cart.Price);
+                }
+                */
+
             }
 
             //IEnumerable<string> longAnimalNames =
-                //from name in animalNames
-                //where name.Length >= 5
-                //orderby name.Length
-                //select name;
+            //from name in animalNames
+            //where name.Length >= 5
+            //orderby name.Length
+            //select name;
 
         }
 
---
+++ dbLesson/ShopDatabaseAdvanced/ShopAdvancedDBContext/AdvancedShopDBContext.cs
@@ -0,0 +1,20 @@
+﻿using ShopDatabaseAdvanced.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDatabaseAdvanced.ShopAdvancedDBContext
+{
+    class AdvancedShopDBContext : DbContext
+    {
+        public AdvancedShopDBContext() : base("AdvancedShopDatabase"){ }
+
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+
+        public DbSet<Food> Foods {get;set;}
+
+    }
+}
